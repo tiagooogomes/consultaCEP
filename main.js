179a65f6bc0672c628f81106c581d1583ab9f4ed
Tiago <tiagooogomes@gmail.com>
2022-02-11 11:58:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,90 @@
+$(function() {
+    $('#botao-consulta').click(consultarCEP);
+    $('input').on('input', verificarValidade);
+    $('#botao-mapa').click(mostrarMapa);
+});
+
+const algumaCoisa = $('#informacoes').html();
+
+function consultarCEP() {
+    const CEP = $('input').val()
+
+   
+    $.ajax({
+        url: `https://cep.awesomeapi.com.br/json/${CEP}`
+    })
+
+    .fail(function() {
+        console.log( "error" );
+        $('#cidade').html('CEP não encontrado')
+      })
+
+    .done(data => {
+        console.log(data);
+        $('#cidade').html(data.city)
+        $('#latitude').html(data.lat);
+        $('#longitude').html(data.lng);
+        $('#ddd').html(`(${data.ddd}) 9 0000-0000`);
+        $('#ibge').html(data.city_ibge);
+    });
+
+    if($('#informacoes').html() !== algumaCoisa){
+
+        $('#informacoes').html(algumaCoisa);
+    }
+
+    aparecerInformacoes();
+    atualizarMostradorCEP();
+ 
+}
+
+function aparecerInformacoes() {
+    $('footer').css('display', 'none');
+    $('#teste').stop().show(600);
+}
+
+function atualizarMostradorCEP() {
+    console.log($('input').val())
+
+    const valor = $('input').val()
+
+    console.log(valor)
+
+    $('#info1').html(valor[0]);
+    $('#info2').html(valor[1]);
+    $('#info3').html(valor[2]);
+    $('#info4').html(valor[3]);
+    $('#info5').html(valor[4]);
+    $('#info6').html(valor[5]); 
+    $('#info6').append(valor[6]); 
+    $('#info6').append(valor[7]); 
+}
+
+function verificarValidade() {
+    const valorDigitado = /^\d{5}-\d{3}$|^\d{8}$/.test($('input').val());
+// console.log($('input').val().length)
+
+
+    // if(valorDigitado === false){
+    //     $('input').removeClass('cor-verde');
+    //     $('input').addClass('cor-vermelha');
+    //     $('label').html('Por favor digite um formato aceito');
+    // } else {
+    //     $('input').removeClass('cor-vermelha');
+    //     $('input').addClass('cor-verde');
+    //     $('label').html('Formato aceito');
+    // }
+
+    // if($('input').val().length === 0) {
+    //     $('input').removeClass('cor-vermelha');
+    //     $('input').removeClass('cor-verde');
+    //     $('label').html('Digite seu CEP sem o traço');
+    // }
+}
+
+
+function mostrarMapa() {
+    // console.log($('#informacoes').html());
+    const cep = $('input').val();
+    $('#informacoes').html(`<iframe src="https://maps.google.com/maps?q=${cep}&t=&z=13&ie=UTF8&iwloc=&output=embed" allowfullscreen="" loading="lazy"></iframe>`);
+}
